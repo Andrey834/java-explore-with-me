b1cf314252558a9e89fdb5fd96c7184ba539eb7d
Andrey M <andry834@yandex.ru>
2023-11-14 16:17:27 +0500

    add stat-gateway, stat-server

---
+++ stat-service/stat-server/src/main/java/ru/practicum/statserver/mapper/EndpointHitMapper.java
@@ -0,0 +1,29 @@
+package ru.practicum.statserver.mapper;
+
+import ru.practicum.statserver.dto.EndpointHitDto;
+import ru.practicum.statserver.model.EndpointHit;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class EndpointHitMapper {
+    public static EndpointHitDto toEndpointHitDto(EndpointHit endpointHit) {
+        return EndpointHitDto.builder()
+                .id(endpointHit.getId())
+                .app(endpointHit.getApp())
+                .uri(endpointHit.getUri())
+                .ip(endpointHit.getIp())
+                .timestamp(endpointHit.getTimestamp().toString())
+                .build();
+    }
+
+    public static EndpointHit toEndpointHit(EndpointHitDto endpointHitDto){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return EndpointHit.builder()
+                .app(endpointHitDto.getApp())
+                .uri(endpointHitDto.getUri())
+                .ip(endpointHitDto.getIp())
+                .timestamp(LocalDateTime.parse(endpointHitDto.getTimestamp(), formatter))
+                .build();
+    }
+}
