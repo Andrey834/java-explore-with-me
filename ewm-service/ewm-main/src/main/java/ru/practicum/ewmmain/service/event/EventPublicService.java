a035e95a6b7f449b8a5143112aeda1f2cf1232f2
Andrey M <andry834@yandex.ru>
2023-11-23 21:56:59 +0500

    remove unused imports

--- ewm-service/ewm-main/src/main/java/ru/practicum/ewmmain/service/event/EventPublicService.java
+++ ewm-service/ewm-main/src/main/java/ru/practicum/ewmmain/service/event/EventPublicService.java
@@ -3,11 +3,9 @@ package ru.practicum.ewmmain.service.event;
 import org.springframework.data.domain.PageRequest;
 import ru.practicum.dto.event.EventFullDto;
 import ru.practicum.dto.event.EventShortDto;
-import ru.practicum.ewmmain.enums.SortEvents;
 import ru.practicum.ewmmain.util.ParamEvents;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventPublicService {
